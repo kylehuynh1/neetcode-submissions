368ed49e6c1122650e9c9f3f6264d6bf4185807e
Kyle Huynh <81531323+kylehuynh1@users.noreply.github.com>
2026-08-10 17:03:08 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for(int i = 0; i < nums.length; i++){
+            int current = nums[i];
+            for(int j = i + 1; j < nums.length; j++){
+                if(current == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
