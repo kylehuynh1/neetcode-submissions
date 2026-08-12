36826935ba55d6edcabc2087cadbdc4fdfce4de9
Kyle Huynh <81531323+kylehuynh1@users.noreply.github.com>
2026-08-12 17:44:44 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] countsS = new int[26];
+        int[] countsT = new int[26];
+
+        //counter letter freq. for string s
+        for(int i = 0; i < s.length(); i++){
+            char current = s.charAt(i);
+            int index = current - 'a';
+            countsS[index]++;
+        }
+
+        //count letter freq. for string t
+        for(int j = 0; j < t.length(); j++){
+            char current = t.charAt(j);
+            int index = current - 'a';
+            countsT[index]++;
+        }
+
+        if(Arrays.equals(countsS, countsT))
+            return true;
+        
+        return false;
+    }
+}
